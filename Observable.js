d33be07ff48db19025f652b78699b8feaa31860f
霍超群 <huochaoqun@cjia.com>
2018-06-10 11:41:25 +0800

    完成练习

--- Observable.js
+++ Observable.js
@@ -11,12 +11,21 @@ class ObserverList {
   }
   add(observer) {
     // todo add observer to list
+    var index = this.observerList.indexOf(observer);
+    if (index < 0) {
+      this.observerList.push(observer);
+    }
   }
   remove(observer) {
     // todo remove observer from list
+    var index = this.observerList.indexOf(observer);
+    if (index >= 0) {
+      this.observerList.splice(index, 1);
+    }
   }
   count() {
     // return observer list size
+    return this.observerList.length;
   }
 }
 
@@ -26,12 +35,17 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer);
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer);
   }
   notify(...args) {
     // todo notify
+    this.observers.observerList.forEach(function (fn) {
+      fn.update(...args);
+    })
   }
 }
 
--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,33 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = [];
+    }
+    if (this.subscribers[type].indexOf(fn) < 0) {
+      this.subscribers[type].push(fn);
+    }
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = [];
+    }
+    var index = this.subscribers[type].indexOf(fn);
+    if (index >= 0) {
+      this.subscribers[type].splice(index, 1);
+    }
   }
 
   publish(type, ...args) {
     // todo publish
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = [];
+    }
+    this.subscribers[type].forEach(function (fn) {
+      fn(...args);
+    })
   }
 
 }
